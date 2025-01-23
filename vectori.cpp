c9ab6ac2129c7aec5d3d06b0c46e13be6096bcf2
Alin <alinalin03030@gmail.com>
2025-01-23 02:05:34 +0000

    vector in c++

--- vectori.cpp
+++ vectori.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 
 
 int main(){ 
+    std::vector <float> nr;
+    for(int i = 0; i < 5; i++) nr.push_back(i);
+    for(auto it = nr.begin(); it != nr.end(); it++) std::cout << *it << std::endl;
 
+    //stergerea elementelor
+    std::vector<int> element;
+    element = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for(auto it = element.begin(); it != element.end();){
+        if(*it % 2 == 0){
+            it = element.erase(it);
+        }else it++;
+    }
+    for(int i : element) std::cout << i << " ";
+
+    //problema 3: inserare si sortare
+    std::vector<int> v1 = {1, 3, 5, 7, 9};
+    int x = 4;
+    auto it = lower_bound(v1.begin(), v1.end(), x);
+    v1.insert(it, x);
+
+    //problema 4: binary search
+    std::vector<int> v4 = {1, 3, 5, 7, 9};
+    int y = 5;
+    bool exists = binary_search(v4.begin(), v4.end(), y);
+    if(exists) std::cout << " Exista in vector " << std::endl;
+    else std::cout << " Nu exista in vector " << std::endl;
 
     
+
+
     std :: vector<int> v;
     v.push_back(10); // adauga element la sfirsitul vectorului
     v.emplace_back(20); // Similar cu push_back, dar mai eficient pentru obiecte complexe.
